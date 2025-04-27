58a0ee879d1399f7346bed11152ec7770aed99e5
Jesus Hernandez <jesushdez@protonmail.com>
2025-04-27 19:46:14 +0200

    Improve job tracking test readability by adding spacing for clarity

--- tests/Features/TrackableJobTest.php
+++ tests/Features/TrackableJobTest.php
@@ -11,12 +11,14 @@ it('can track a job', function () {
     expect($id)->not->toBeNull();
 
     $jobStatus = JobStatus::find($id);
+
     expect($jobStatus)->not->toBeNull();
     expect($jobStatus->status)->toBe(JobStep::DISPATCHING->value);
     expect($jobStatus->result)->toBeNull();
 
     $job->handle();
     $jobStatus->refresh();
+
     // expect($jobStatus->status)->toBe(JobStep::COMPLETED->value);
     expect($jobStatus->result)->toBe(4);
 });
