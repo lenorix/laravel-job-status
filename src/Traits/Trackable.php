1158bf1e6a3133e8a934946ded517befe4cfba3a
Jesus Hernandez <jesushdez@protonmail.com>
2025-04-28 00:25:48 +0200

    Refactor trackDispatch method to utilize track method for job tracking initialization

--- src/Traits/Trackable.php
+++ src/Traits/Trackable.php
@@ -8,26 +8,64 @@ use Illuminate\Queue\Attributes\WithoutRelations;
 use Lenorix\LaravelJobStatus\Enums\JobStep;
 use Lenorix\LaravelJobStatus\Models\JobTracker;
 
+/**
+ * Track a job and allow to get a result in the future.
+ *
+ * After `use` this trait, call `$this->track()` in the job constructor,
+ *  or use `trackDispatch()` static method to dispatch and track.
+ *
+ * The tracker is in `$job->tracker` property, has an `id` and
+ *  can be used with `JobTracker::find($id)` to get the job status
+ *  and result.
+ */
 trait Trackable
 {
     use Dispatchable;
 
+    /**
+     * Use it only to read, not modify it to ensure the track
+     *  of the job works correctly.
+     *
+     * @var JobTracker|null
+     */
     #[WithoutRelations]
     public ?JobTracker $tracker = null;
 
+    /**
+     * Dispatch and track at the same time.
+     *
+     * @param  mixed  ...$arguments
+     * @return PendingDispatch
+     */
     public static function trackDispatch(mixed ...$arguments): PendingDispatch
     {
         $dispatch = static::dispatch(...$arguments);
 
-        $tracker = JobTracker::create([
-            'status' => JobStep::DISPATCHING,
-        ]);
-        $job = $dispatch->getJob();
-        $job->tracker = $tracker;
+        $dispatch->getJob()->track();
 
         return $dispatch;
     }
 
+    /**
+     * Idempotently track the job.
+     *
+     * @return void
+     */
+    protected function track(): void
+    {
+        if (is_null($this->tracker)) {
+            $this->tracker = JobTracker::create([
+                'status' => JobStep::DISPATCHING,
+            ]);
+        }
+    }
+
+    /**
+     * Let the job give a result by the tracker model.
+     *
+     * @param  mixed  $result
+     * @return void
+     */
     protected function setResult(mixed $result): void
     {
         if ($this->tracker) {
