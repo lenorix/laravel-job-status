a47e54bab63f7c4128c4bfe6efc6bdbf65425789
Jesus Hernandez <jesushdez@protonmail.com>
2025-04-28 11:48:57 +0200

    Refactor tracker method documentation for clarity and usage guidance

--- src/Traits/Trackable.php
+++ src/Traits/Trackable.php
@@ -29,6 +29,21 @@ trait Trackable
     #[WithoutRelations]
     public ?JobTracker $tracker = null;
 
+    /**
+     * Don't use it directly from the job, use `setResult()`
+     *  to set the result instead.
+     *
+     * It's used to track a job after calling `dispatch()`
+     *  like:
+     *
+     * ```php
+     * $tracker = DummyJob::dispatch(2)
+     *     ->getJob()
+     *     ->tracker();
+     * ```
+     *
+     * @return JobTracker
+     */
     public function tracker(): JobTracker
     {
         $this->track();
